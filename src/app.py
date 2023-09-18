1067d637b519b5dccc8f1dc8b3662981d8739d42
Mpineiro20 <130186047+Mpineiro20@users.noreply.github.com>
2023-09-18 22:09:40 +0000

    first commit

--- src/app.py
+++ src/app.py
@@ -15,6 +15,36 @@ CORS(app)
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
+# create dictionaires to add to jackson family list (array)
+# python a list of dictionaries is = in js an array of objects
+# create 3 dictionaries of family members in the instructions and add them to jackson family. 
+
+jackson_family.add_member({
+    'id':jackson_family._generateId(),
+    'first-name': 'John',
+    'last-name':'Jackson' ,
+    'age':33 ,
+    'lucky_numbers':[7,13,22]
+
+})
+jackson_family.add_member({
+    'id':jackson_family._generateId(),
+    'first-name': 'Jane',
+    'last-name':'Jackson' ,
+    'age':35 ,
+    'lucky_numbers':[10,14,3]
+
+})
+jackson_family.add_member({
+    'id':jackson_family._generateId(),
+    'first-name': 'Jimmy',
+    'last-name':'Jackson' ,
+    'age':5 ,
+    'lucky_numbers':[1]
+
+})
+
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -25,20 +55,24 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
+
+
+
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def get_all_family_members():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+   return jsonify(members), 200
 
+@app.route ('/member/<int:member_id>',methods= ['GET]')
+def get_single_family_member(member_id):
+    member = jackson_family.get_member(member id)
+        return jsonify(),200
 
-    return jsonify(response_body), 200
-
-# this only runs if `$ python src/app.py` is executed
-if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=True)
+@app.route('/member', methods=['POST'])
+def handle_add_member():
+    data = request.json
+    jackson_family.add_member(data)
+    return jsonify(data), 200
--- src/datastructures.py
+++ src/datastructures.py
@@ -18,19 +18,33 @@ class FamilyStructure:
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
         return randint(0, 99999999)
+    
 
-    def add_member(self, member):
-        # fill this method and update the return
-        pass
 
-    def delete_member(self, id):
-        # fill this method and update the return
-        pass
-
-    def get_member(self, id):
-        # fill this method and update the return
-        pass
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
+
+      
+
+    def get_member(self, id):
+        # fill this method and update the return
+        # need a for loop to retrieve a specific family member id to display
+      
+            for member in self._members:
+             if member['id'] == int(id):
+                 return member
+
+        
+
+
+
+    def add_member(self, member):
+        # fill this method and update the return
+        # push
+        self. _members.append(member)
+        
+    def delete_member(self, id):
+        # fill this method and update the return
+        # need a for loop to find specific family member id to remove
